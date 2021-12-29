0baff8445d755710cef9c66b18ae5cc0b636e0cd
RaviMauryaHootowl <ravimaurya027@gmail.com>
2021-12-29 20:11:35 +0530

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser=require('body-parser');
 var cors = require('cors');
 const api = require('./api');
   
-const port=3000;
+const port=process.env.PORT || 3000;
 const app=express();
 
 app.use(cors())
